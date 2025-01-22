db3dbca032ee1d9dc3d0bbeadd41f6ec5c2112dd
JesusC1224 <ydjkings@gmail.com>
2025-01-21 20:29:36 -0500

    First version of Bank library
    asdf

---
+++ BankLibrary/BankLibrary/CheckingAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public class CheckingAccount : Account
+    {
+        public decimal overdraftLimit {  get; set; }
+        public CheckingAccount(String accountNumber, string accountName, decimal initialBalance, decimal overDraftLimit) : base(accountNumber, accountName, initialBalance)
+        {
+            overDraftLimit = overDraftLimit
+        }
+        public override void Withdraw(decimal amount)
+        {
+            if (amount <= 0) throw new ArgumentException("Withdrawl amount must be positive");
+            if (amount > Balance + overdraftLimit) throw new InvalidOperationException("Overdraft limit exceeded");
+            this.Withdraw(amount);
+        }
+    }
+}
---
+++ BankLibrary/BankLibrary/HighInterestSavingsAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public class HighInterestSavingsAccount : savingsAccount
+    {
+        public HighInterestSavingsAccount(string accountNumber, string accountName, decimal initialBalance, decimal interestRate) : base(accountNumber, accountName, initialBalance, interestRate) 
+        {
+
+        }
+        public void ApplybonucInterest(Decimal bonusRate)
+        {
+            decimal bonusInterestPayment = this.Balance * bonusRate;
+            this.Deposit(bonusInterestPayment);
+        }
+    }
+}
